65f5ad174558a3fa252c01728f4badfd3ae5db6f
rkdals213 <rkdals213@naver.com>
2022-01-04 21:35:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mysololife/auth/IntroActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mysololife.auth
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.mysololife.R
+import com.example.mysololife.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.joinBtn.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
