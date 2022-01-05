1956ee00837288b964481c0da2b217e9757829ef
rkdals213 <rkdals213@naver.com>
2022-01-05 22:43:17 +0900

    ㅁㄴㅇ

--- app/src/main/java/com/example/mysololife/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mysololife/auth/LoginActivity.kt
@@ -1,12 +1,60 @@
 package com.example.mysololife.auth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.mysololife.MainActivity
 import com.example.mysololife.R
+import com.example.mysololife.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        binding.loginBtn.setOnClickListener {
+            var isGoToJoin = true
+
+            val email = binding.emailArea.text.toString()
+            val password = binding.passwordArea.text.toString()
+
+            if (email.isEmpty()) {
+                Toast.makeText(this, "이메일을 입력하세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            if (password.isEmpty()) {
+                Toast.makeText(this, "Password을 입력하세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            if (isGoToJoin) {
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
+
+                            val intent = Intent(this, MainActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, "실패", Toast.LENGTH_LONG).show()
+                        }
+                    }
+            }
+        }
     }
 }
